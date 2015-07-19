fd2c720feb6b3ef808823b749b338dea49011aff
Jay Wengrow <jaywngrw@gmail.com>
2015-07-19 09:26:21 -0500

    allow user to add task

--- app/assets/javascripts/tasks_ctrl.js
+++ app/assets/javascripts/tasks_ctrl.js
@@ -5,6 +5,9 @@
     
     $scope.tasks = ["Mow the lawn", "Take out trash", "Feed the cat"];
 
+    $scope.addTask = function(task) {
+      $scope.tasks.push(task);
+    };
 
     window.scope = $scope;
   });
