b4925fbf06b7afe3c5ad0b210d8f509fc4373b0d
Jay Wengrow <jaywngrw@gmail.com>
2015-07-19 09:34:15 -0500

    only add task if there is text in textbox

--- app/assets/javascripts/tasks_ctrl.js
+++ app/assets/javascripts/tasks_ctrl.js
@@ -6,8 +6,10 @@
     $scope.tasks = ["Mow the lawn", "Take out trash", "Feed the cat"];
 
     $scope.addTask = function(task) {
-      $scope.tasks.push(task);
-      $scope.newTask = null;
+      if(task) {
+        $scope.tasks.push(task);
+        $scope.newTask = null;
+      }
     };
 
     window.scope = $scope;
